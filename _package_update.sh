cac943401a2fff2932177cbe121f3c1af3c149f5
Alexandr Nefedov <alexandr.nefedvo.b2w@gmail.com>
2022-01-06 11:29:34 +0200

    Script descrition

--- _package_update.sh
+++ _package_update.sh
@@ -7,7 +7,7 @@ ansible()
 
 if ((`wc -c < ansible.log` > 51200)); then
 	echo "Attention! Ansible log is more then 50kb.
-	0) Clear logs/ansible.log and continue.
+	0) Clear ansible.log and continue.
 	1) Ignore and continue.
 	Any other key) Exit."
 	read -p "Choose your action [0-2]:" x
